9bda5650d2c345b52f509d6724b9c707cb0b4bcf
Joseph Pasia <joseph.pasia@gmail.com>
2026-05-04 21:23:13 +0200

    minor edit

--- pkg_credit_default/config/config_loader.py
+++ pkg_credit_default/config/config_loader.py
@@ -4,11 +4,11 @@ from pkg_credit_default.utils.logger import logger
 from pkg_credit_default.config.config_paths import BASE_CONFIG_PATH, ENV_CONFIG_PATH
 
 def _load_yaml(path):
-    logger.info(f"load_yaml() - Loading YAML configuration from: {path}")
+    logger.info(f"_load_yaml() - Loading YAML configuration from: {path}")
     with open(path, "r") as f:
         return yaml.safe_load(f)
 
-def _load_config():
+def load_config():
     logger.info("load_config() - Starting to load configuration...")
     base = _load_yaml(BASE_CONFIG_PATH)
     env  = _load_yaml(ENV_CONFIG_PATH)
@@ -16,4 +16,4 @@ def _load_config():
     logger.info("load_config() - Configuration loaded successfully.")
     return always_merger.merge(base, env)
 
-CONFIG = _load_config()
+CONFIG = load_config()
