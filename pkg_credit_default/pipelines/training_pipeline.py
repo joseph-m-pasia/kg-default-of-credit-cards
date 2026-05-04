6b94d5e58f03efad7a25f4743610110795293988
Joseph Pasia <joseph.pasia@gmail.com>
2026-05-04 21:37:50 +0200

    load the config and read the raw data

--- pkg_credit_default/pipelines/training_pipeline.py
+++ pkg_credit_default/pipelines/training_pipeline.py
@@ -1,12 +1,18 @@
-from pkg_credit_default.config.loader import config_loader
-from pkg_credit_default.data.loaders.raw_loader import raw_loaders
+from pkg_credit_default.config.config_loader import load_config
+from pkg_credit_default.data.loaders.raw_loader import load_data_from_csv
+from pkg_credit_default.utils.logger import logger
 
 def run_training():
+
+    logger.info("run_training() - Starting training pipeline...")
+
     # STEP 1: CONFIG
     config = load_config()
 
+    print(config)
+
     # STEP 2: RAW DATA
-    df = load_raw_data(config)
+    df = load_data_from_csv(config["data"]["raw_path"])
 
     # STEP 3: FEATURES
     # X, y = build_features(df, config)
@@ -14,4 +20,7 @@ def run_training():
     # STEP 4: MODEL TRAINING
     # model = train_model(X, y, config)
 
+    model = config
     return model
+
+run_training()
