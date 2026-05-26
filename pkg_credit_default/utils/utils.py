fcc68dc0b742dfcce8dc3ec75a35a3f90688581d
Joseph Pasia <joseph.pasia@gmail.com>
2026-05-26 14:52:57 +0200

    contains helper functions

---
+++ pkg_credit_default/utils/utils.py
@@ -0,0 +1,28 @@
+from pkg_credit_default.utils.logger import logger
+from datetime import datetime
+
+from pyexpat import model 
+
+import os   
+import joblib
+import importlib
+from typing import Any, Dict
+
+
+def save_model(model, model_dir, model_type="", timestamp=True) -> str:
+    """
+    Save the trained model to disk.
+    """
+    logger.info(f"Saving model '{model_type}' to disk...")
+
+    tstamp = datetime.now().strftime("%Y%m%d_%H%M%S") if timestamp else ""
+
+    model_dir = os.path.join(model_dir, tstamp + f"_{model_type}")  # e.g. "outputs/models/20240101_120000_logistic_regression"
+    os.makedirs(model_dir, exist_ok=True)                           # create folder if it doesn't exist
+
+    model_path = os.path.join(model_dir, f"{model_type}_model.pkl")
+    joblib.dump(model, model_path)
+  
+    logger.info(f"Model saved to {model_path}")
+
+    return model_path  
