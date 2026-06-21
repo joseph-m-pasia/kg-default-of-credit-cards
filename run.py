b1394a8c0bb3ae017bb3920aae0473204f35ce98
Joseph Pasia <joseph.pasia@gmail.com>
2026-06-21 22:36:24 +0200

    minor update in run

--- run.py
+++ run.py
@@ -5,4 +5,6 @@ from pkg_credit_default.pipelines.training_pipeline import run_training_pipeline
 #==================== LAUNCH THE TRAINING PIPELINE ==============================    
 list_of_models = ["logistic_regression", "random_forest", "xgb_regressor", "knn", "lightgbm"]
 
-run_training_pipeline(model_type=list_of_models)
+champion_model = run_training_pipeline(model_type=list_of_models, params={"save_output": True, "plot_metrics": False, "save_split_data": True})               
+
+logger.info(f"Champion model based on F1 score: {champion_model['champion_model'][0]} with F1 score = {champion_model['champion_model'][1]['best_score']:.4f}") 
