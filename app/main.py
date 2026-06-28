7c932e8b6315762459cfd1e47ac13054618a77d4
Joseph Pasia <joseph.pasia@gmail.com>
2026-06-28 21:00:49 +0200

    included the fetaure names

--- app/main.py
+++ app/main.py
@@ -9,17 +9,20 @@ from app.schemas import (
 
 app = FastAPI()
 
-model = None
-
+model_bundle = None
 
 def get_model():
-    global model
 
-    if model is None:
-        model = joblib.load("artifacts/model.pkl")
+    global model_bundle
+    if model_bundle is None:
+        # Load the bundle saved by save_model()
+        model_bundle = joblib.load("artifacts/model.pkl")
 
-    return model
+        print("Model loaded successfully.")
+        print("Bundle keys:", model_bundle.keys())  # should show: model, feature_names
 
+    # Return both the model and feature names
+    return model_bundle["model"], model_bundle["feature_names"]
 
 @app.post(
     "/predict",
@@ -28,19 +31,20 @@ def get_model():
 def predict(
     data: PredictionRequest
 ):
+    # load the model and feature names
+    model, feature_names = get_model()
 
-    model = get_model()
-
+    # Create a DataFrame from the input data
     df = pd.DataFrame(
         [data.model_dump()]
     )
 
-    prediction = model.predict(df)[0]
+    # Ensure the DataFrame has the same columns as the model was trained on
+    df = df[feature_names]
 
-    probability = (
-        model
-        .predict_proba(df)[0][1]
-    )
+    # Predict the outcome
+    prediction = model.predict(df)[0]
+    probability = model.predict_proba(df)[0][1]
 
     return PredictionResponse(
         prediction=int(prediction),
